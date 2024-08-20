fda4ce479d704f5d39b05c9fae452b52c3650a14
Yunsik Han <yunsikhan.yh@gmail.com>
2024-08-21 11:02:30 +1200

    started adding health display system for player

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.VFX;
+
+public class Health : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private VisualElement[] hearts;
+
+    [SerializeField] private Sprite emptyHeartSprite;
+    [SerializeField] private Sprite filledHeartSprite;
+
+    public void Init(int maxHeartCount)
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/PlayerCharacter.cs
+++ Assets/Scripts/PlayerCharacter.cs
@@ -5,15 +5,26 @@ using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour
-{
+{   
+    //movement
     public InputAction MoveAction;
     Vector2 move;
     public float speed = 3.0f;
+
+    //health
+    public int health { get { return currentHealth; } }
+    public int maxHealth = 5;
+    public int currentHealth;
+
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
 
     // Start is called before the first frame update
     void Start()
     {
         MoveAction.Enable();
+        //GetComponent<HealthUI>().Init(maxHealth);
     }
 
     // Update is called once per frame
@@ -24,5 +35,23 @@ public class PlayerController : MonoBehaviour
         Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
         transform.position = position;
     }
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
+
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+
+
 
 }
